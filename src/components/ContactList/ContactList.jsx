7bcd8497ce211550e4c42c1038b108398310efaf
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-09-03 12:21:03 +0200

    ContactList update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import {
+  selectContacts,
   selectError,
   selectFilteredContacts,
 } from "../../redux/contacts/selectors";
@@ -9,15 +10,16 @@ import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 
 const ContactList = () => {
-  const visibleContacts = useSelector(selectFilteredContacts);
+  const visibleFilteredContacts = useSelector(selectFilteredContacts);
+  const visibleContacts = useSelector(selectContacts);
   const error = useSelector(selectError);
   return (
     <div>
       <ContactForm />
       {visibleContacts.length > 0 && <SearchBox />}
       <ul className={css.list}>
-        {visibleContacts.length > 0 ? (
-          visibleContacts.map((item) => (
+        {visibleFilteredContacts.length > 0 || visibleContacts.length ? (
+          visibleFilteredContacts.map((item) => (
             <li className={css.contact} key={item.id}>
               <Contact contacts={item} />
             </li>
